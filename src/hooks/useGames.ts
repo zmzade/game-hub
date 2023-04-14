ca08be9d302d78ccac9a55cd0b65d1ca42d61420
zmzade <zmahmoodzadeh@yahoo.com>
2023-04-14 15:48:17 +0200

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,10 +1,18 @@
 import React from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
