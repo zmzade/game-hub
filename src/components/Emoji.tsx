28ca04b1f6d98328a6739bc07c95ceeae2f2abcf
zmzade <zmahmoodzadeh@yahoo.com>
2023-04-17 19:50:37 +0200

    Ship genres with the app

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -6,13 +6,16 @@ import { Image, ImageProps } from "@chakra-ui/react";
 interface Props {
   rating: number;
 }
+
 const Emoji = ({ rating }: Props) => {
   const emojiMap: { [key: number]: ImageProps } = {
     3: { src: meh, alt: "meh", boxSize: "25px" },
     4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
     5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
   };
+
   if (rating < 3) return null;
+
   return <Image {...emojiMap[rating]} marginTop={1} />;
 };
 
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -16,8 +16,11 @@ interface Props {
 }
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
+
   if (error) return null;
+
   if (isLoading) return <Spinner />;
+
   return (
     <>
       <Heading fontSize="2xl" marginBottom={3}>
